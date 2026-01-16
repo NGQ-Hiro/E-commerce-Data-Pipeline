f9a61e4bb3bfcba531595ee0bc0961611383c01c
quanghieu <ngo089120@gmail.com>
2026-01-16 22:50:25 +0700

    fix: final cdc

--- postgres/init/03_snap_shot.sh
+++ postgres/init/03_snap_shot.sh
@@ -19,8 +19,12 @@ END
 echo "Create logical replication slot (for CDC later)..."
 
 psql -U $DB_USER -d $DB_NAME -c "
-SELECT 'snap_shot' FROM pg_replication_slots WHERE slot_name = 'snap_shot'
-UNION ALL
-SELECT pg_create_logical_replication_slot('snap_shot', 'pgoutput')
-WHERE NOT EXISTS (SELECT 1 FROM pg_replication_slots WHERE slot_name = 'snap_shot');" \
-> /dev/null 2>&1
+DO \$\$
+BEGIN
+  IF NOT EXISTS (
+    SELECT 1 FROM pg_replication_slots WHERE slot_name = 'snap_shot'
+  ) THEN
+    PERFORM pg_create_logical_replication_slot('snap_shot', 'pgoutput');
+  END IF;
+END
+\$\$;"
