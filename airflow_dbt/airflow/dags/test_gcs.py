72f7ffa17e8d055b1a9813589c9d2ab57f40e5db
Hieu <ngo089120@gmail.com>
2026-01-23 10:43:58 +0700

    test: autodetect

---
+++ airflow_dbt/airflow/dags/test_gcs.py
@@ -0,0 +1,47 @@
+import os
+import logging
+import psycopg2
+import tempfile
+from psycopg2.extras import LogicalReplicationConnection
+from airflow.decorators import dag, task
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.providers.google.cloud.hooks.gcs import GCSHook
+from airflow.providers.google.cloud.sensors.gcs import GCSObjectsWithPrefixExistenceSensor
+from airflow.providers.google.cloud.hooks.bigquery import BigQueryHook
+from google.cloud import bigquery
+
+# --- CONFIGURATION ---
+GCP_CONN_ID = os.getenv('GCP_CONN_ID', 'google_cloud_default')
+POSTGRES_CONN_ID = os.getenv('POSTGRES_CONN_ID', 'postgres_default')
+BUCKET_NAME = os.getenv('BUCKET_NAME', 'olist-ecommerce-bucket')
+PROJECT_ID = os.getenv('PROJECT_ID', 'e-commerce-484010')
+BRONZE = os.getenv('BRONZE', 'bronze')
+PUBLICATION_NAME = os.getenv('PUBLICATION_NAME', 'dbz_publication')
+SLOT_NAME = os.getenv('SLOT_NAME', 'snap_shot')
+
+@dag(schedule=None, catchup=False)
+def test():
+    @task
+    def create_external_snapshot_tables_task():
+        # MOVE CLIENT INSIDE TASK & INPUT IS STRING (NOT LIST)
+        table_name = 'product_category_name_translation'
+        bq_hook = BigQueryHook(gcp_conn_id=GCP_CONN_ID)
+        client = bq_hook.get_client(project_id=PROJECT_ID)
+
+        # Config format external table
+        ext_cfg_csv = bigquery.ExternalConfig("CSV")
+        ext_cfg_csv.source_uris = [f"gs://{BUCKET_NAME}/{table_name}/snapshot/{table_name}.csv"]
+        ext_cfg_csv.autodetect = True
+        ext_cfg_csv.options.skip_leading_rows = 1
+        # Optional: Uncomment below if needed for special cases like order_reviews
+        ext_cfg_csv.options.field_delimiter = ','
+        ext_cfg_csv.options.quote_character = '"'
+        ext_cfg_csv.options.allow_quoted_newlines = True
+        
+        table_ref = bigquery.Table(f"{PROJECT_ID}.{BRONZE}.{table_name}_snapshot_external")
+        table_ref.external_data_configuration = ext_cfg_csv
+        client.create_table(table_ref, exists_ok=True)
+        logging.info(f"Created Snapshot Table for {table_name}")
+
+    create_external_snapshot_tables_task()
+test()
